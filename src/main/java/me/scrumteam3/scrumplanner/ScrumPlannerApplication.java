e3a0c0cc9a52c5bacd096956d34a6ed5bcbd4d5e
lucas-maijers <lucas.maijers@student.rocvf.nl>
2021-05-25 12:37:37 +0200

    Generation of Spring backend

---
+++ src/main/java/me/scrumteam3/scrumplanner/ScrumPlannerApplication.java
@@ -0,0 +1,13 @@
+package me.scrumteam3.scrumplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/scrumteam3/scrumplanner/ScrumPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.scrumteam3.scrumplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
