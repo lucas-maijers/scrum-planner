4e50fbfce769c5f4ca9de1f4384e15bd43010135
just-in-and-out <jdelaat@roc-dev.com>
2021-06-08 09:52:02 +0200

    Update new_task_java.js
    fixed so it correctly displays task below eachother. still does not allow adding tasks

--- new_task_java.js
+++ new_task_java.js
@@ -1,13 +1,16 @@
 let Tasks = ['clean your dam computer jerold ' ,'design logo ','write javascript code '];
 
+var submitted_task = document.getElementById("Newtask");
+
 function add_task(){
-  Tasks.push("new task")
+  Tasks.push(submitted_task)
 }
 
 var paragraph = document.getElementById("Plannertasks");
-var linebreak = document.createElement("br");
-Tasks.forEach(function(item, index, array) {
+
+Tasks.forEach(function(item, index) {
   var task_identify = document.createTextNode(item);
+  var linebreak = document.createElement("br");
   paragraph.appendChild(task_identify);
   paragraph.appendChild(linebreak);
 })
